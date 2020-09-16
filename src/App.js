7d363f89d6550ab982327ae5e8390f229d581c04
DeepeshRohit <69351728+DeepeshRohit@users.noreply.github.com>
2020-09-16 20:50:58 +0530

    assignment2
    Table

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React  from 'react';
 import './App.css';
-
-function App() {
+import * as ReactBootstrap from 'react-bootstrap';
+const App = () =>
+{
+  const employee = [
+  { name: "Charlie   "  ,job: "Janitor" },
+  { name: "Mac    "  ,job: "  Bouncer" },
+  { name: "Dee   "  , job: "  Aspiring Actoress" },
+  { name: "Dennis "  , job: "  Bartender" }
+]
+  const renderEmployee = (employee,index) =>{
+    return(
+    <tr key = "index">
+      <td>{employee.name}</td>
+      <td>{employee.job}</td>
+    </tr>
+    )
+  }
   return (
     <div className="App">
-       <h1 >
-         Hello World!
-        </h1>
+   <ReactBootstrap.Table striped bordered hover variant="dark">
+  <thead>
+    <tr>
+      <th>Name  </th>
+      <th>Job   </th>
+    </tr>
+  </thead>
+  <tbody>
+   
+   {employee.map(renderEmployee)}
+ 
+  </tbody>
+</ReactBootstrap.Table>
     </div>
   );
 }
 
-export default App;
 
+export default App;
